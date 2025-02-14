40bd04d691583c646e11bda9ff87aa59bf2d43a9
Tsuki12 <Njabuloo.masilela@gmail.com>
2025-02-14 21:59:28 +0200

    pass an additional arg as the file name when initalising

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     match args[1].as_str() {
-        "init" => init(),
+        "init" => init(if args.len() > 2 {
+            Some(args[2].as_str())
+        } else {
+            None
+        }),
         _ => eprintln!("command not supported"),
     }
 }
 
-fn init() {
+fn init(repo_name: Option<&str>) {
     // check if a git folder has already been created or not
     let current_dir = env::current_dir()
         .unwrap()
@@ -20,7 +24,13 @@ fn init() {
         .into_string()
         .unwrap();
 
-    let git_dir = current_dir + "/.test";
+    let full_name = match repo_name {
+        Some(name) => format!("/{}/.git", name),
+        None => "/.git".to_string(),
+    };
+
+    let git_dir = current_dir + full_name.as_str();
+    println!("{git_dir}");
     let is_initialized = Path::new(&git_dir).is_dir();
 
     if is_initialized {
@@ -28,28 +38,40 @@ fn init() {
         return;
     }
 
-    let cmds: Vec<Vec<&str>> = vec![
-        vec!["mkdir", ".test"], // create an empty .test dir
+    if repo_name.is_some() {
+        let _ = execute_programs(
+            String::from("mkdir"),
+            Some(vec![String::from(repo_name.unwrap())]),
+        );
+    }
+
+    let cmds: Vec<Vec<String>> = vec![
+        vec![String::from("mkdir"), git_dir.clone()], // create an empty .test dir
         // files and dirs under the .test dir
-        vec!["touch", ".test/config"],
-        vec!["touch", ".test/description"],
-        vec!["touch", ".test/HEAD"],
-        vec!["mkdir", ".test/hooks"],
-        vec!["mkdir", ".test/info"],
-        vec!["mkdir", ".test/objects"],
-        vec!["mkdir", ".test/refs"],
+        vec![String::from("touch"), format!("{}/config", git_dir.clone())],
+        vec![
+            String::from("touch"),
+            format!("{}/description", git_dir.clone()),
+        ],
+        vec![String::from("touch"), format!("{}/HEAD", git_dir.clone())],
+        vec![String::from("mkdir"), format!("{}/hooks", git_dir.clone())],
+        vec![String::from("mkdir"), format!("{}/info", git_dir.clone())],
+        vec![
+            String::from("mkdir"),
+            format!("{}/objects", git_dir.clone()),
+        ],
+        vec![String::from("mkdir"), format!("{}/refs", git_dir.clone())],
     ];
 
     for cmd in &cmds {
-        match execute_programs(cmd[0], Some(cmd[1..].to_vec())) {
+        match execute_programs(String::from(cmd[0].as_str()), Some(cmd[1..].to_vec())) {
             Ok(_v) => (),
             Err(e) => panic!("Error initialising GIT repository: {e}"),
         }
     }
-    
 }
 
-fn execute_programs(cmd: &str, args: Option<Vec<&str>>) -> Result<String, String> {
+fn execute_programs(cmd: String, args: Option<Vec<String>>) -> Result<String, String> {
     let output;
     match args {
         Some(args) => output = Command::new(cmd).arg(args.join(" ")).output(),
